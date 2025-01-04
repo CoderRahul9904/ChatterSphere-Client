82d5e134b0b7840a9f2556b94ac272822489d1d2
CoderRahul9904 <mouryarahul543@gmail.com>
2025-01-04 17:49:33 +0530

    done

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+    return (
+        <div className="w-screen flex justify-center items-center">
+            <footer className="absolute bottom-4 text-sm dark:text-gray-400">
+               © 2025 ChatterSphere. All rights reserved.
+           </footer>
+        </div>
+    )
+}
+
+export default Footer
